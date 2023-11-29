3d3648810b2d4573e81da90d12587716743df2b7
Gregory <astgregory@bk.ru>
2023-11-30 02:41:20 +0400

    Gunicorn

--- app/django_htmx/settings.py
+++ app/django_htmx/settings.py
@@ -128,7 +128,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / "static"
+
+# url директории медиа файлов
+MEDIA_URL = "/media/"
+# Путь до директории медиа файлов
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
